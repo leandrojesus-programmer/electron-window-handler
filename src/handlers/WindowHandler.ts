7070646df52d8ec0daf34015c9799e3abcd9fc48
Leandro Jesus <48989252+leandrojesus-programmer@users.noreply.github.com>
2019-03-31 15:25:00 +0100

    WindowHandler class code refactor

--- src/handlers/WindowHandler.ts
+++ src/handlers/WindowHandler.ts
@@ -1,28 +1,42 @@
 import { IWindowHandler } from "./IWindowHandler";
+import { BrowserWindow, BrowserWindowConstructorOptions } from "electron";
 
 export class WindowHandler implements IWindowHandler {
-    VIEW_PATH: string;    browserWindows: [string, Electron.BrowserWindow][];
+    
+    VIEW_PATH: string;    
+    browserWindows: [string, BrowserWindow][];
+
+    constructor(){
+        this.VIEW_PATH = ""
+        this.browserWindows = []
+    }
+
     showWindow(view: string, id?: number | undefined): void {
         throw new Error("Method not implemented.");
     }
+
     hideWindow(view: string, id?: number | undefined): void {
         throw new Error("Method not implemented.");
     }
+
     closeWindow(view: string, id?: number | undefined): void {
         throw new Error("Method not implemented.");
     }
-    createWindow(view: string, options?: Electron.BrowserWindowConstructorOptions | undefined): void {
-        throw new Error("Method not implemented.");
-    }
-    deleteWindow(view: string, id?: number | undefined): void {
-        throw new Error("Method not implemented.");
-    }
-    browserWindowExist(view: string, id?: number | undefined): boolean {
-        throw new Error("Method not implemented.");
-    }
-    getBrowserWindow(view: string, id?: number | undefined): Electron.BrowserWindow | null {
+
+    createWindow(view: string, options?: BrowserWindowConstructorOptions | undefined): void {
         throw new Error("Method not implemented.");
     }
 
+    deleteWindow(view: string, id?: number | undefined): void {
+        throw new Error("Method not implemented.");
+    }
+
+    browserWindowExist(view: string, id?: number | undefined): boolean {
+        throw new Error("Method not implemented.");
+    }
+
+    getBrowserWindow(view: string, id?: number | undefined): BrowserWindow | null {
+        throw new Error("Method not implemented.");
+    }
 
 }
